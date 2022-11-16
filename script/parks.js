175d51ac414f07e245f9239f8659876423c38a50
Tenzing Sherpa <tenzingsherpa@Tenzings-MacBook-Pro.local>
2022-11-16 14:23:44 -0500

    search by locaion complete

--- script/parks.js
+++ script/parks.js
@@ -11,9 +11,15 @@ window.onload = function () {
     let searchByParkType = document.getElementById("searchByParkType");
     searchByParkType.onchange = searchByParkOnChange;
 
+    let resultLocation = document.getElementById("resultLocation");
+    resultLocation.onchange = displayResultOnChange;
+
+
+
 
     document.getElementById("searchByLocation").style.display = "none";
     document.getElementById("searchByParkType").style.display = "none";
+    document.getElementById("resultLocation").style.display = "none";
 
 }
 
@@ -21,6 +27,8 @@ function addDropdowns() {
 
     document.getElementById("searchByLocation").style.display = "none";
     document.getElementById("searchByParkType").style.display = "none";
+    document.getElementById("parksDescription").style.display = "none";
+    document.getElementById("resultLocation").style.display = "none";
 
     let SearchFilter = document.getElementById("SearchFilter").value;
 
@@ -41,10 +49,7 @@ function addDropdowns() {
     else if (SearchFilter == "") {
         document.getElementById("searchByLocation").style.display = "none";
         document.getElementById("searchByParkType").style.display = "none";
-
     }
-
-
 
 }
 
@@ -55,7 +60,7 @@ function addOptionsOnLocation() {
 
     let newOption = document.createElement("option");
     newOption.value = "";
-    newOption.text = "Search By Location";
+    newOption.text = "Please Select a State";
     searchByLocation.appendChild(newOption);
 
     for (let location of locationsArray) {
@@ -90,30 +95,45 @@ function addOptionsOnPark() {
 
 function searchByLocationOnChange() {
 
+   document.getElementById("parksDescription").style.display = "none";
+
     let searchByLocation = document.getElementById("searchByLocation").value;
-    let parksDescription = document.getElementById("parksDescription");
+
+    let result = document.getElementById("resultLocation")
+
+    result.length = 0;
+
+    let newOption = document.createElement("option");
+    newOption.value = "";
+    newOption.text = "Please Select a Park " ;
+    result.appendChild(newOption);
 
     for (let place of nationalParksArray) {
 
         if (searchByLocation == place.State) {
-            parksDescription.style.display = "block"
-            parksDescription.innerHTML = "<span style='color: white;'>Name : </span>" + place.LocationName + "<br/>" + "<span style='color: white;'>Address : </span>" + place.Address + "<br/>" + "<span style='color: white;'>City : </span>" + place.City + "<br/>" + "<span style='color: white;'>State : </span>" + place.State + "<br/>" + "<span style='color: white;'>Zip Code : </span>" + place.ZipCode;
+
+            let paragraph = document.createElement("p");
+            let newOption = document.createElement("option");
+            newOption.value = place.LocationName;
+            newOption.text = place.LocationName;
+            result.appendChild(newOption);
+
+            document.getElementById("myDiv").appendChild(paragraph);
+
+            result.style.display = "block";
 
         } else if (searchByLocation == "") {
 
-            parksDescription.style.display = "none"
+            result.style.display = "none"
         }
 
     }
 
 
 
-
-
-
-
-
 }
+
+
 
 function searchByParkOnChange() {
 
@@ -122,15 +142,48 @@ function searchByParkOnChange() {
 
     for (let park of nationalParksArray) {
 
+        if (searchByParkType) {
 
-            
+
+
+            parksDescription.style.display = "block"
+            parksDescription.innerHTML = "<span style='color: white;'>Name : </span>" + park.LocationName + "<br/>" + "<span style='color: white;'>Address : </span>" + park.Address + "<br/>" + "<span style='color: white;'>City : </span>" + park.City + "<br/>" + "<span style='color: white;'>State : </span>" + park.State + "<br/>" + "<span style='color: white;'>Zip Code : </span>" + park.ZipCode;
+
+        }
+
+
+
 
     }
 
 
 
+}
 
 
+function displayResultOnChange() {
+
+    let result = document.getElementById("resultLocation");
+    let parksDescription = document.getElementById("parksDescription");
+
+    for (let park of nationalParksArray) {
+
+        if (result.value == park.LocationName) {
+
+            console.log("hello")
+
+            parksDescription.style.display = "block"
+            parksDescription.innerHTML = "<span style='color: white;'>Name : </span>" + park.LocationName + "<br/>" + "<span style='color: white;'>Address : </span>" + park.Address + "<br/>" + "<span style='color: white;'>City : </span>" + park.City + "<br/>" + "<span style='color: white;'>State : </span>" + park.State + "<br/>" + "<span style='color: white;'>Zip Code : </span>" + park.ZipCode;
+
+        } else if (result.value == "") {
+            parksDescription.style.display = "none"
+
+        }
+
+
+
+
+    }
 
 
 
